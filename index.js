ae86fb9ff59d05b82870380f2faab1a8e37f1224
Enayet <enayetflweb@gamil.com>
2024-04-23 20:57:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+var videoshow = require('videoshow')
+
+var images = [
+  "1.png",
+  "2.png",
+  "3.png",
+  "4.png",
+]
+
+var videoOptions = {
+  fps: 25,
+  loop: 5, // seconds
+  transition: true,
+  transitionDuration: 1, // seconds
+  videoBitrate: 1024,
+  videoCodec: 'libx264',
+  size: '640x?',
+  audioBitrate: '128k',
+  audioChannels: 2,
+  format: 'mp4',
+  pixelFormat: 'yuv420p'
+}
+
+videoshow(images, videoOptions)
+
+  .save('video.mp4')
+  .on('start', function (command) {
+    console.log('ffmpeg process started:', command)
+  })
+  .on('error', function (err, stdout, stderr) {
+    console.error('Error:', err)
+    console.error('ffmpeg stderr:', stderr)
+  })
+  .on('end', function (output) {
+    console.error('Video created in:', output)
+  })
